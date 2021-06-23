c761fbf40df6a79e0f5de4496234274a1b3a1edb
Filipe Melo <filipe.melo@liferay.com>
2021-06-23 11:29:50 -0300

    LSW-29 Created routing module

--- modules/liferay-sample-module/liferay-sample-module-js-web/src/app/app.module.ts
+++ modules/liferay-sample-module/liferay-sample-module-js-web/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {SampleModule} from "sample/sample.module";
+import { AppRoutingModule } from './app-routing.module';
+import { SampleDetailComponent } from './sample/components/sample-detail/sample-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SampleDetailComponent
   ],
   imports: [
     BrowserModule,
-    SampleModule
+    SampleModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
