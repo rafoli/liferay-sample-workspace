170e63c0aa520c7cccdaeb17608d3ed0b9ccfeda
Filipe Melo <filipe.melo@liferay.com>
2021-06-23 11:29:27 -0300

    LSW-29 Created sample detail component

---
+++ modules/liferay-sample-module/liferay-sample-module-js-web/src/app/sample/components/sample-detail/sample-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sample-detail',
+  templateUrl: './sample-detail.component.html',
+  styleUrls: ['./sample-detail.component.scss']
+})
+export class SampleDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
