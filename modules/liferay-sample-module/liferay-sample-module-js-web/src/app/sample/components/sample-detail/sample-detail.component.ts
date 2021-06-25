2e24e4f99995640b5e292d52fcddc7d5aee31a85
Filipe Melo <filipe.melo@liferay.com>
2021-06-25 12:06:46 -0300

    LSW-29 created sample detail CRUD actions

--- modules/liferay-sample-module/liferay-sample-module-js-web/src/app/sample/components/sample-detail/sample-detail.component.ts
+++ modules/liferay-sample-module/liferay-sample-module-js-web/src/app/sample/components/sample-detail/sample-detail.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, } from '@angular/core';
+import { Observable } from 'rxjs';
+import {Sample} from "sample/model/sample";
+import {SampleService} from "../../service/sample.service";
+import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-sample-detail',
@@ -6,10 +11,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sample-detail.component.scss']
 })
 export class SampleDetailComponent implements OnInit {
+  @Input() sample?: Sample;
 
-  constructor() { }
+  constructor(private sampleService: SampleService, private route: ActivatedRoute, private location: Location, private router: Router) { }
 
   ngOnInit(): void {
+    this.getSample();
+  }
+
+  goBack(): void {
+    this.router.navigateByUrl("", {skipLocationChange: true})
+  }
+
+  getSample() {
+    const id = Number(this.route.snapshot.paramMap.get('id'))
+    this.sampleService.getSample(id)
+      .subscribe(sample=>this.sample=sample)
+  }
+
+  save() {
+    if (this.sample) {
+      this.sampleService.updateSample(this.sample)
+        .subscribe(()=>this.goBack());
+    }
   }
 
 }
