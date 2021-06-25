57096ea2442fc9170e4f7bd6c312f440c353e729
Filipe Melo <filipe.melo@liferay.com>
2021-06-25 12:23:34 -0300

    LSW-29 calling sampleService functions

--- modules/liferay-sample-module/liferay-sample-module-js-web/src/app/sample/components/sample-list/sample-list.component.ts
+++ modules/liferay-sample-module/liferay-sample-module-js-web/src/app/sample/components/sample-list/sample-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {SampleService} from "../../service/sample.service";
 import {Sample} from "sample/model/sample";
 import {Observable} from "rxjs";
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-sample-list',
   templateUrl: './sample-list.component.html',
@@ -12,11 +12,20 @@ export class SampleListComponent implements OnInit {
 
   samples?: Observable<Sample[]>;
 
-  constructor(private _sampleService: SampleService) { }
+  constructor(private _sampleService: SampleService, private router: Router) { }
 
   ngOnInit(): void {
     console.log("CALLED HERE!!")
     this.samples = this._sampleService.getSamples()
   }
+  create() {
+    this.router.navigateByUrl("create", {skipLocationChange: true});
+  }
 
+  delete(id: string) {
+    if (id) {
+      this._sampleService.deleteSample(Number(id))
+        .subscribe(()=>{});
+    }
+  }
 }
