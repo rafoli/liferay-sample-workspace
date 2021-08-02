fcb185c264d0a1934d916a181d51bb8f79a8be68
Elmano Neto <elmanoneto@gmail.com>
2021-08-02 15:50:06 -0300

    dev: theme with namespace

--- modules/liferay-sample-module/liferay-sample-module-react-web/src/modules/Sample/create.js
+++ modules/liferay-sample-module/liferay-sample-module-react-web/src/modules/Sample/create.js
@@ -15,6 +15,7 @@ export default function Create({ createSample }) {
     }
 
     return (
+<<<<<<< HEAD
         <div className="form-group">
             <form onSubmit={(e) => createNewSample(sampleName, e)}>
                 <div className="input-group">
@@ -31,6 +32,14 @@ export default function Create({ createSample }) {
                         <button disabled={!sampleName} className="btn btn-primary" id="btn-add-sample">Add</button>
                     </div>
                 </div>
+=======
+        <div className="dsd-app">
+            <form onSubmit={(e) => createNewSample(sampleName, e)}>
+                <input type="text" placeholder="Name" value={sampleName} onChange={e => setSampleName(e.target.value)} />
+                <button disabled={!sampleName} className="dsd__btn-primary">Add</button>
+                <button disabled={!sampleName} className="dsd btn-primary">Add</button>
+                <button disabled={!sampleName} className="btn-primary">Add</button>
+>>>>>>> bd416d0 (dev: theme with namespace)
             </form>
         </div>
     )
