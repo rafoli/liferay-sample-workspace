39195bfe1d73881a869fd858b940fb4015d34ad2
Rennan Prysthon <rennan.prysthon@liferay.com>
2021-08-18 10:20:33 -0300

    fix: added missing angular modules on spec classes for testing errors

--- modules/liferay-sample-module/liferay-sample-module-js-web/src/app/sample/components/sample-detail/sample-detail.component.spec.ts
+++ modules/liferay-sample-module/liferay-sample-module-js-web/src/app/sample/components/sample-detail/sample-detail.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing'
 
 import { SampleDetailComponent } from './sample-detail.component';
 
@@ -8,6 +10,7 @@ describe('SampleDetailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientModule, RouterTestingModule],
       declarations: [ SampleDetailComponent ]
     })
     .compileComponents();
