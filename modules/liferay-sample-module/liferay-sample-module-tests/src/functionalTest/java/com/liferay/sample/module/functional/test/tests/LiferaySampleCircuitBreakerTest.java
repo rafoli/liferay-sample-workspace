fe3b094892fe7b1d28887b11c88d3584c94843ee
afablf <andre.batista@liferay.com>
2021-08-23 10:20:30 -0300

    LSW-51 Dependencies and test class
    Test class using REST Assured and Junit.

--- modules/liferay-sample-module/liferay-sample-module-tests/src/functionalTest/java/com/liferay/sample/module/functional/test/tests/LiferaySampleCircuitBreakerTest.java
+++ modules/liferay-sample-module/liferay-sample-module-tests/src/functionalTest/java/com/liferay/sample/module/functional/test/tests/LiferaySampleCircuitBreakerTest.java
@@ -1,30 +1,32 @@
 package com.liferay.sample.module.functional.test.tests;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertEquals;
-import com.jayway.restassured.*;
+
+import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.specification.RequestSpecification;
-import com.liferay.sample.module.ws.config.RestAPIConfiguration;
+
+import org.junit.jupiter.api.Test;
 
 public class LiferaySampleCircuitBreakerTest {
 
-	RestAPIConfiguration restAPIConfiguration;
-
 	@Test
 	public void mustEnterClosedStateWhenMicroserviceFail () {
-		RestAssured.baseURI = "http://localhost:8080/";
+		
+		RestAssured.baseURI = "http://localhost:8080"; //_restAPIConfiguration.apiBaseUrl();
 		RestAssured.basePath = "o/sample-module/samples";
 		
+		RequestSpecification httpRequest = RestAssured.given();
+
 		String responseMsg = "";
 		
-		RequestSpecification httpRequest = RestAssured.given();
-		
 		for (int i = 0; i < 4; i++) {
+			
 			responseMsg = httpRequest.given()
 							.header("Accept", "application/json")
 							.header("Authorization", "Basic c2FtcGxlLnRlc3RAbGlmZXJheS5jb206dGVzdA==")
 							.get("")
 							.asString();
+
 		}	
 		
 		assertEquals(true, responseMsg.contains("CircuitBreaker"));
