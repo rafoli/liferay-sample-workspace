260b63cffd55969703a728d2d99e4cc2abcd3a5c
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-09-10 18:52:44 -0300

    LSW-52 Format source within web-rest module

--- modules/liferay-sample-module/liferay-sample-module-web-rest/src/main/java/com/liferay/sample/module/web/rest/SampleApplication.java
+++ modules/liferay-sample-module/liferay-sample-module-web-rest/src/main/java/com/liferay/sample/module/web/rest/SampleApplication.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
+
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -23,7 +24,6 @@ import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -36,103 +36,116 @@ import org.slf4j.LoggerFactory;
  * @author Rafael Oliveira
  */
 @Component(
-        property = {
-                JaxrsWhiteboardConstants.JAX_RS_APPLICATION_BASE + "=/sample-module",
-                JaxrsWhiteboardConstants.JAX_RS_NAME + "=SampleModule.Rest"
-        },
-        service = Application.class
+	property = {
+		JaxrsWhiteboardConstants.JAX_RS_APPLICATION_BASE + "=/sample-module",
+		JaxrsWhiteboardConstants.JAX_RS_NAME + "=SampleModule.Rest"
+	},
+	service = Application.class
 )
 @Produces(MediaType.APPLICATION_JSON)
 public class SampleApplication extends Application {
-	
-    public Set<Object> getSingletons() {
-        return Collections.singleton(this);
-    }
 
-    @Path("/samples")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getSamples(@Context HttpServletRequest request) throws SampleApplicationException {
-    	
-    	_logger.debug("Getting all samples available...");
-    	
-    	try {
-    		List<SampleObject> samples = _sampleService.getSamples();
-            
-            if (samples.isEmpty()) {
-            	throw new SampleApplicationException("No samples available!");
-            } else {
-            	_logger.info(samples.size() + " samples available");
-            	
-            	return Response.ok(Page.of(samples)).build();
-            }
-    	} catch (Exception error) {
-    		_logger.error(new Date() + " :: " + _logger.getName() + " :: " + error.getMessage());
-    		return Response.status(Status.BAD_REQUEST.getStatusCode()).entity(error.getMessage()).build();
-    	}      
-
-    }
-
-    @Path("/samples/{id}")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getSample(@Context HttpServletRequest request, @PathParam("id") String id) {
-
-        SampleObject sample = _sampleService.getSample(id);
-
-        return Response.ok(JSONFactoryUtil.looseSerialize(sample)).build();
-
-    }
-
-    @Path("/samples")
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response addSample(@Context HttpServletRequest request, String body) {
-    	
+	@Path("/samples")
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addSample(@Context HttpServletRequest request, String body) {
 		SampleObject sample = JSONFactoryUtil.looseDeserialize(body, SampleObject.class);
-		
-        sample = _sampleService.addSample(sample);
-        
-        return Response.ok(JSONFactoryUtil.looseSerialize(sample)).build();            
-    	
-    }
 
-    @Path("/samples")
-    @PUT
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response updateSample(@Context HttpServletRequest request, String body) {
+		sample = _sampleService.addSample(sample);
 
-    	SampleObject sample = JSONFactoryUtil.looseDeserialize(body, SampleObject.class);
-    	
-        SampleObject s = _sampleService.getSample(sample.getId());
+		return Response.ok(
+			JSONFactoryUtil.looseSerialize(sample)
+		).build();
+	}
 
-        if(s == null)
-            return Response.status(Response.Status.NOT_FOUND).build();
-        else
-        	sample = _sampleService.updateSample(sample);
-        	return Response.ok(JSONFactoryUtil.looseSerialize(sample)).build();
-        	
-    }
+	@DELETE
+	@Path("/samples/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response deleteSample(@Context HttpServletRequest request, String id) {
+		SampleObject s = _sampleService.getSample(id);
 
-    @Path("/samples/{id}")
-    @DELETE
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response deleteSample(@Context HttpServletRequest request, String id) {
+		if (s == null)
 
-        SampleObject s = _sampleService.getSample(id);
+			return Response.status(
+				Response.Status.NOT_FOUND
+			).build();
 
-        if(s == null)
-            return Response.status(Response.Status.NOT_FOUND).build();
+		_sampleService.deleteSample(id);
 
-        _sampleService.deleteSample(id);
+		return Response.status(
+			Response.Status.OK
+		).build();
+	}
 
-        return Response.status(Response.Status.OK).build();
-    }
-    
-    // declare at the end, just like References annotations (Liferay best practices)
-    private static final Logger _logger = LoggerFactory.getLogger(SampleApplication.class.getName());
+	@GET
+	@Path("/samples/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getSample(@Context HttpServletRequest request, @PathParam("id") String id) {
+		return Response.ok(
+			JSONFactoryUtil.looseSerialize(_sampleService.getSample(id))
+		).build();
+	}
 
-    @Reference
-    private SampleService _sampleService;
+	@GET
+	@Path("/samples")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getSamples(@Context HttpServletRequest request) throws SampleApplicationException {
+		_logger.debug("Getting all samples available...");
+
+		try {
+			List<SampleObject> samples = _sampleService.getSamples();
+
+			if (samples.isEmpty()) {
+				throw new SampleApplicationException("No samples available!");
+			}
+
+			_logger.info(samples.size() + " samples available");
+
+			return Response.ok(
+				Page.of(samples)
+			).build();
+		}
+		catch (Exception exception) {
+			_logger.error(new Date() + " :: " + _logger.getName() + " :: " + exception.getMessage());
+
+			return Response.status(
+				Response.Status.BAD_REQUEST.getStatusCode()
+			).entity(
+				exception.getMessage()
+			).build();
+		}
+	}
+
+	public Set<Object> getSingletons() {
+		return Collections.singleton(this);
+	}
+
+	@Path("/samples")
+	@Produces(MediaType.APPLICATION_JSON)
+	@PUT
+	public Response updateSample(@Context HttpServletRequest request, String body) {
+		SampleObject sample = JSONFactoryUtil.looseDeserialize(body, SampleObject.class);
+
+		SampleObject s = _sampleService.getSample(sample.getId());
+
+		if (s == null)
+
+			return Response.status(
+				Response.Status.NOT_FOUND
+			).build();
+			else
+			sample = _sampleService.updateSample(sample);
+
+		return Response.ok(
+			JSONFactoryUtil.looseSerialize(sample)
+		).build();
+	}
+
+	// declare at the end, just like References annotations (Liferay best practices)
+
+	private static final Logger _logger = LoggerFactory.getLogger(SampleApplication.class.getName());
+
+	@Reference
+	private SampleService _sampleService;
 
 }
