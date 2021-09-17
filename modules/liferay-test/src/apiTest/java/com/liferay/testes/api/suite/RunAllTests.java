fdd84d3448336b8776dc09af72ecc7c8a43831ba
afablf <andre.batista@liferay.com>
2021-09-17 18:20:49 -0300

    INLSW-12 Create the test module

---
+++ modules/liferay-test/src/apiTest/java/com/liferay/testes/api/suite/RunAllTests.java
@@ -0,0 +1,14 @@
+package com.liferay.testes.api.suite;
+
+import com.liferay.tests.api.tests.LiferaySampleCircuitBreakerTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * @author Andre Batista
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses(LiferaySampleCircuitBreakerTest.class)
+public class RunAllTests {
+}
