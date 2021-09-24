dfa22758afc6bf8b2cca26545fb1e0e9d2f31ee8
Rennan Prysthon <rennan.prysthon@liferay.com>
2021-09-24 15:31:26 -0300

    INLSW-39: refact: Recreate react module using cra

--- modules/liferay-sample-module/liferay-sample-module-clay-react/src/App.js
+++
@@ -1,9 +0,0 @@
-import Samples from './modules/Sample/index';
-
-function App() {
-	return (
-		<Samples />
-	)
-}
-
-export default App;
---
+++ modules/liferay-sample-module/liferay-sample-module-clay-react/src/App.tsx
@@ -0,0 +1,3 @@
+const App = () => <h1>Liferay Sample Clay React Module</h1>;
+
+export default App;
