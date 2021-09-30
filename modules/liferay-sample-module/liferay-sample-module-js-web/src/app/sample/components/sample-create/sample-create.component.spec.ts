d8419496358f6622ee9520e77c20dc47fcf3b3fd
Rennan Prysthon <rennan.prysthon@liferay.com>
2021-09-30 18:16:16 -0300

    INLSW-33 test: Create test file for sample-list and refact other spec files

--- modules/liferay-sample-module/liferay-sample-module-js-web/src/app/sample/components/sample-create/sample-create.component.spec.ts
+++ modules/liferay-sample-module/liferay-sample-module-js-web/src/app/sample/components/sample-create/sample-create.component.spec.ts
@@ -1,17 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+
+import { Sample } from 'sample/model/sample';
 
 import { SampleCreateComponent } from './sample-create.component';
+import { SampleService } from 'sample/service/sample.service';
 
 describe('SampleCreateComponent', () => {
   let component: SampleCreateComponent;
   let fixture: ComponentFixture<SampleCreateComponent>;
 
   beforeEach(async () => {
+    const sample: Sample = {
+      id: '1',
+      name: "Sample 1"
+    };
+    const sampleService = jasmine.createSpyObj('SampleService', ['getSample'])
+
+    sampleService.getSample.and.returnValue(of(sample));
+
     await TestBed.configureTestingModule({
       imports: [HttpClientModule, RouterTestingModule],
-      declarations: [ SampleCreateComponent ]
+      declarations: [ SampleCreateComponent ],
+      providers: [
+        {
+          provide: SampleService,
+          useValue: sampleService
+        }
+      ]
     })
     .compileComponents();
   });
--- modules/liferay-sample-module/liferay-sample-module-js-web/src/app/sample/components/sample-detail/sample-detail.component.spec.ts
+++ modules/liferay-sample-module/liferay-sample-module-js-web/src/app/sample/components/sample-detail/sample-detail.component.spec.ts
@@ -2,16 +2,34 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing'
 
+import { Sample } from 'sample/model/sample';
+
+import { of } from 'rxjs';
+
 import { SampleDetailComponent } from './sample-detail.component';
+import { SampleService } from 'sample/service/sample.service';
 
 describe('SampleDetailComponent', () => {
   let component: SampleDetailComponent;
   let fixture: ComponentFixture<SampleDetailComponent>;
 
   beforeEach(async () => {
+    const sample: Sample = {
+      id: '1',
+      name: "Sample 1"
+    };
+
+    const sampleService = jasmine.createSpyObj('SampleService', ['getSample'])
+
+    sampleService.getSample.and.returnValue(of(sample));
+
     await TestBed.configureTestingModule({
       imports: [HttpClientModule, RouterTestingModule],
-      declarations: [ SampleDetailComponent ]
+      declarations: [ SampleDetailComponent ],
+      providers: [{
+        provide: SampleService,
+        useValue: sampleService
+      }]
     })
     .compileComponents();
   });
