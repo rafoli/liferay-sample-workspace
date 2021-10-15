e69d112feefa0c8bb3b7ee094bc20865fb46c934
Rennan Prysthon <rennan.prysthon@liferay.com>
2021-10-15 13:25:50 -0300

    fix: Fix Create form regex validation

--- modules/liferay-sample-module/liferay-sample-module-react-web/src/components/Create/index.js
+++ modules/liferay-sample-module/liferay-sample-module-react-web/src/components/Create/index.js
@@ -12,7 +12,7 @@ export default function Create() {
     }
 
     const isFormInvalid = useMemo(() => {
-        const regex = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+        const regex = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
         return (sampleName.trim().length === 0 || regex.test(sampleName))
     } ,[sampleName])
     
